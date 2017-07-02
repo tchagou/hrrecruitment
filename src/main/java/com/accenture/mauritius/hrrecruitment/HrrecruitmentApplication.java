0d981e8c6741116a251a4c9a917db2ecae702a17
tchagou <tchagou2e@gmail.com>
2017-07-03 00:25:54 +0400

    Add files via upload

---
+++ src/main/java/com/accenture/mauritius/hrrecruitment/HrrecruitmentApplication.java
@@ -0,0 +1,12 @@
+package com.accenture.mauritius.hrrecruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrrecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrrecruitmentApplication.class, args);
+	}
+}
---
+++ src/test/java/com/accenture/mauritius/hrrecruitment/HrrecruitmentApplicationTests.java
@@ -0,0 +1,16 @@
+package com.accenture.mauritius.hrrecruitment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrrecruitmentApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
